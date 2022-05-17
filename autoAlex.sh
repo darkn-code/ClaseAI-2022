ee3365fd62e110a911dc37cef27178bba6e7ad4f
AlexMunizFlores <alejandromunizflores940414@gmail.com>
2022-05-17 00:12:53 -0500

    comandos Alejandro

--- autoAlex.sh
+++ autoAlex.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git pull origin master --allow-unrelated-histories
 python Alejnadro.py
+
+git pull origin master --allow-unrelated-histories
 git add "basedatos/datos.csv"
 git commit -m "Datos Exteriores Alejandro"
 git push origin master
