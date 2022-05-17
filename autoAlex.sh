2c948c8628ad10233059a362fa7bfc45c802be83
AlexMunizFlores <alejandromunizflores940414@gmail.com>
2022-05-17 00:20:19 -0500

    Datos Exteriores Alejandro

--- autoAlex.sh
+++ autoAlex.sh
@@ -2,5 +2,6 @@
 Alejnadro.py
 
 git pull origin master --allow-unrelated-histories
-git commit -am "Datos Exteriores Alejandro"
+git add .
+git commit -m "Datos Exteriores Alejandro"
 git push origin master
