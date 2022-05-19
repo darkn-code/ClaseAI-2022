36c3efa44aa938e2ea94f25348114a4266ee7c4c
Alex <alejandromunizflores940414@gmail.com>
2022-05-19 15:55:57 -0500

    sh

--- Comandos/autoAlex.sh
+++ Comandos/autoAlex.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
 cd Alex
 git pull origin master 
-=======
-cd Jennifer
-git pull origin master --allow-unrelated-histories
->>>>>>> d975377af9bf6751b5a17c1face6fc1430def4e9
 python3 Alejnadro.py
 python3 Alejnadro.py
 git add .
