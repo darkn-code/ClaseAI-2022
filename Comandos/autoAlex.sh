81b30f76243f817bbfbf657700512b8023e97325
Alex <alejandromunizflores940414@gmail.com>
2022-05-23 21:02:20 -0500

    Datos Exteriores Alejandro

--- Comandos/autoAlex.sh
+++ Comandos/autoAlex.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cd ..
-git pull origin master 
+cd ~/Alex/
+pwd
 python3 Alejnadro.py
 python3 Alejnadro.py
 git add .
 git commit -m "Datos Exteriores Alejandro"
+git pull origin master
 git push origin master
