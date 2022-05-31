0073ec09051ef779f8b1d3145ebbfaabfa410d46
Alex <alejandromunizflores940414@gmail.com>
2022-05-30 23:45:10 -0500

    Datos Exteriores Alejandro

--- concatenadoDatosAMF.py
+++ concatenadoDatosAMF.py
@@ -4,7 +4,7 @@ import datetime
 import random
 import time
 
-DATA_BASE = './basedatos/datos.csv'
+DATA_BASE = './basedatos/blockAlex.txt'
 dirDataBase = "./basedatos/{}"
 
 def revisarArchivo(archivo):
@@ -24,9 +24,26 @@ def main():
             tablaDatos.drop(["Unnamed: 0"],axis=1,inplace=True)
         with open(DATA_BASE,'r',encoding='utf-8') as f:
             datos = f.readlines()
-            print("tamaño datos")
-            print(numpy.shape(datos))
-            print("fin")
+        x = len(datos)
+        for ind in range(x):
+            #print(datos[ind])
+            tem = datos[ind].split(',')[0]
+            #print(tem)
+            hum = datos[ind].split(',')[1]
+            #print(hum)
+            noS = datos[ind].split(',')[2]
+            #print(noS)
+            dat = datos[ind].split(',')[3]
+            #print(dat)
+            hou = datos[ind].split(',')[4]
+            #print(hou)
+            luv = datos[ind].split(',')[5]
+            #print(luv)
+            newData = {"S1:Temperatura":tem,"S2:Humedad":hum,"No Serie":noS,"Fecha":dat,"Hora":hou,"Lluvia":luv}
+            #print(newData)
+            tablaDatos = tablaDatos.append(newData,ignore_index=True)
+            print(tablaDatos)
+            tablaDatos.to_csv((dirDataBase).format("datos.csv",index=False))
     except:
         print("XD")
 
