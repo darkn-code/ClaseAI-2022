915cfc37ba66ce0d75ba9a93722607519b6fe79e
Alex <alejandromunizflores940414@gmail.com>
2022-05-30 22:29:32 -0500

    Datos Exteriores Agrupados  Alejandro

---
+++ Comandos/autoBlockAMF.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/Alex
+python3 alexInfoBlock.py
+python3 alexInfoBlock.py
+git add .
+git commit -m "Datos Exteriores Agrupados  Alejandro"
+git pull origin master
+git push origin master
